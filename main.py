29b3b2e34a26606225d75dbb852a1616d9d0c00c
tz1006 <a@omg.tf>
2017-12-27 20:23:25 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,35 @@
+# !/Python
+
+import requests
+import sqlite3
+import threading
+import time
+from bs4 import BeautifulSoup
+from datetime import date
+from datetime import datetime
+
+################################
+
+proxies = {
+    "https": "http://165.227.100.201:80",
+    "https": "http://183.235.254.159:8080",
+    "https": "http://183.235.254.159:8080"
+}
+proxies = None
+
+################################
+
+import tools
+
+import shares
+import sort
+import watchdog
+
+
+
+
+
+
+import code
+code.interact(banner = "", local = locals())
+
